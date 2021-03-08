15819425ae3db95f7cead3de50d3306c00624e5f
devikagulavani <80191753+devikagulavani@users.noreply.github.com>
2021-03-08 12:52:02 +0530

    Add files via upload

---
+++ ecommerce.js
@@ -0,0 +1,25 @@
+//var name= "Devika";
+//var ID= 9212;
+//var Add = "Satyam Takala Kolhapur";
+//var Class= "BSC COMPUTER";
+//var Res=(` ${name} is name of student with id= ${ID} and her address is ${Add} with class ${Class}` );
+//var len = Res.length;
+//console.log(`Lenght of string is :${len} `);
+//console.log(Res);
+let emp=
+{
+    name : "DEVIKA",
+    College : "VCK"
+
+}
+console.log(emp);
+alert("PLEASE SIGNUP TO BLAZE !");
+
+
+let name=prompt("Please Enter Your Name", "Name");
+  
+let email=prompt("Please Enter Your Email", "@gmail");
+alert("Thank You For Subscribing to blaze");
+console.log(name + email);
+
+
